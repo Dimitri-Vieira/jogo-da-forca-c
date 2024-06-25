ff021dc490d119131cbed4f8edffadc0f1375aa0
Dimitri Kael Pires Vieira <dimitripiriswork@gmail.com>
2024-06-24 22:23:15 -0300

    continuando a modularizar o código
    Modularização do código e criação de variáveis globais

--- forca.c
+++ forca.c
@@ -1,50 +1,46 @@
 #include <stdio.h>
 #include <string.h>
 
+// VARIÁVEIS GLOBAIS
+char palavraSecreta[20]; // array que armazena a palavra secreta
+char chutes[26]; // array que armazena os chutes de letras que o usuário já deu
+int numTentativas = 0; // número que servirá como índice de posição para o array "chutes" para que seja possível guardar o chute realizado em uma posição específica do array, preenchendo o array com os caracteres chutados
+
 void abertura(){
     printf("*****************************\n");
     printf("*       Jogo da Forca       *\n");
     printf("*****************************\n\n");
 }
 
-void capturaChute(char chutes[26], int numTentativas){
+void capturaChute(){
     char chute;
     scanf(" %c", &chute);
 
     // armazenando o chute (letra) feito, dentro do array "chutes" na posição que o número da tentativa corresponder, incrementando este número em todo final do loop
     chutes[numTentativas] = chute;
+    numTentativas++;
+    
 }
 
+int jaChutou(char palavraSecreta){
+    int achou = 0;
 
-int main(){
-    char palavraSecreta[20];
+    // varrendo o array "chutes" o comparando com o array "palavraSecreta" para verificar se há algum caractér igual entre eles
+    for(int j = 0; j < numTentativas; j++){
+        if(chutes[j] == palavraSecreta){
+            achou = 1;
+            break;
+        }
+    }
+    return achou;
+}
 
-    // escreve uma string inteira dentro do array (é acrescentado o caracter \0 ao final da string)
-    sprintf(palavraSecreta, "MELANCIA");
-
-    int acertou = 0;
-    int enforcou = 0;
-
-    char chutes[26]; // array que armazena os chutes de letras que o usuário já deu
-    int numTentativas = 0; // número que servirá como índice de posição para o array "chutes" para que seja possível guardar o chute realizado em uma posição específica do array, preenchendo o array com os caracteres chutados
-
-    // imprime o cabeçalho do jogo
-    abertura();
-
-    do{ 
-        
+void desenhandoForca(){
         // varrendo o array "palavraSecreta"
         for(int i = 0; i < strlen(palavraSecreta); i++){
 
-            int achou = 0;
-
-            // varrendo o array "chutes" o comparando com o array "palavraSecreta" para verificar se há algum caractér igual entre eles
-            for(int j = 0; j < numTentativas; j++){
-                if(chutes[j] == palavraSecreta[i]){
-                    achou = 1;
-                    break;
-                }
-            }
+            // verifica se o caractér inserido já foi chutado
+            int achou = jaChutou(palavraSecreta[i]);
 
             if(achou){
                 printf("%c ", palavraSecreta[i]);
@@ -54,10 +50,31 @@ int main(){
             }
         }
         printf("\n");
+}
+
+void escolhePalavra(){
+    // escreve uma string inteira dentro do array (é acrescentado o caracter \0 ao final da string)
+    sprintf(palavraSecreta, "MELANCIA");
+}
+
+int main(){
+    int acertou = 0;
+    int enforcou = 0;
+
+    
+    // escolhe a palavra secreta da forca
+    escolhePalavra();
+
+    // imprime o cabeçalho do jogo
+    abertura();
+
+    do{ 
+        
+        // desenha a forca
+        desenhandoForca();
 
         // registra um novo chute no array "chutes"
-        capturaChute(chutes, numTentativas);
-        numTentativas++;
+        capturaChute();
 
     }while(!acertou && !enforcou); // condição formulada com lógica booleana. A "!" indica negação, ou seja, inversão
 
