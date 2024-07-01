b59391714c616fffda24cf8a524975dc35da39ed
Dimitri Kael Pires Vieira <dimitripiriswork@gmail.com>
2024-07-01 20:15:38 -0300

    Upload de arquivo de cabeçalho forca.h
    Fazendo upload do arquivo de cabeçalho que possui as funções e constantes usadas no arquivo principal forca.c

---
+++ forca.h
@@ -0,0 +1,10 @@
+// arquivo criado e instânciado com todas as funções e constantes utilizadas no arquivo "forca.c" para que não precisemos nos preocupar com a ordem que devemos dispô-las no arquivo "froca.c". Dessa forma, criando uma espécie de biblioteca.
+
+void abertura();
+void capturaChute();
+void desenhandoForca();
+void escolhePalavra();
+int acertou();
+int enforcou();
+int jaChutou(char letra);
+void adicionaPalavra();
